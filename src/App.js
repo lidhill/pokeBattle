759438ca13e84930ce552a3d04995483be58302d
unknown <alelid95gmail.com>
2023-11-11 17:49:00 +0100

    fetching from api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 //App.js
-import React, { useState } from 'react';
+// App.js
+// App.js
+import React, { useState, useEffect } from 'react';
 import PokemonList from './PokemonList';
 import SearchBox from './SearchBox';
 import Scroll from './Scroll';
-import { pokemons } from './pokemons'; // Import the pokemons data
 import './App.css';
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
   const [searchfield, setSearchfield] = useState('');
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=900');
+        const data = await response.json();
+
+        // Fetch additional details for each Pokemon
+        const pokemonDetails = await Promise.all(
+          data.results.map(async (pokemon) => {
+            const pokemonResponse = await fetch(pokemon.url);
+            const pokemonData = await pokemonResponse.json();
+            return {
+              id: pokemonData.id,
+              name: pokemonData.name,
+            };
+          })
+        );
+
+        setPokemons(pokemonDetails);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchPokemons();
+  }, []); // Empty dependency array ensures the effect runs once when the component mounts
 
   const onSearchChange = (event) => {
     setSearchfield(event.target.value);
